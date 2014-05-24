b6b9c57a4bca65b0322a14b28b4b7478d87ce454
neuromancer <x@y.z>
2014-05-24 00:30:36 -0700

    added support for RWeka

--- ipython_notebook.sh
+++ ipython_notebook.sh
@@ -1,1 +1,2 @@
-ipython  notebook $1 --ip=0.0.0.0 --no-browser
+#LD_LIBRARY_PATH=/usr/lib/jvm/java-7-openjdk-i386/jre/lib/i386/server:${LD_LIBRARY_PATH}
+R CMD javareconf -e "ipython  notebook $1 --ip=0.0.0.0 --no-browser"
