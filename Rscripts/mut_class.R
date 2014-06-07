e99c9bca91ade329569d18ac6a4308c8165ab482
neuromancer <x@y.z>
2014-06-07 14:19:31 -0700

    iReworked test script

--- Rscripts/mut_class.R
+++ Rscripts/mut_class.R
@@ -37,104 +37,83 @@ mut_dm_df["class"] = cats
 
 sink()
 
-msizes = rev(c(1, 50, 100, 150, 200, 250, 300))
+msizes = (rev(c(1, 50, 100, 150, 200, 250, 300)))
 res = c()
 
 mut_robust_cases = mut_dm_df[mut_dm_df$class == "R",]
 mut_buggy_cases  = mut_dm_df[mut_dm_df$class == "B",]
 
-for (n in msizes){
+aug_robust_cases = mut_robust_cases 
+aug_buggy_cases  = mut_buggy_cases
+
+for (n in msizes) {
+
+  nrep = 3
+  mut_only_acc = 0.0 
   
-  print(c("n:",n))
+  #print(c("n:",n))
 
-  mut_robust_cases = mut_robust_cases[mut_robust_cases$size >= n,]
-  mut_buggy_cases = mut_buggy_cases[mut_buggy_cases$size >= n,]
+  #mut_robust_cases = mut_robust_cases[mut_robust_cases$size >= n,]
+  #mut_buggy_cases = mut_buggy_cases[mut_buggy_cases$size >= n,]
   
-  robust_cases = mut_robust_cases[,names(mut_robust_cases) != "size"]
-  buggy_cases  = mut_buggy_cases[,names(mut_buggy_cases) != "size"]
+  robust_cases = aug_robust_cases[aug_robust_cases$size >= n, names(aug_robust_cases) != "size"]
+  buggy_cases  = aug_buggy_cases[aug_buggy_cases$size >= n,   names(aug_buggy_cases) != "size"]
 
-  n = length(uniq_programs)
-  psample = sample(n)
-  train_programs = uniq_programs[psample[1:as.integer(n*0.75)]]
-  test_programs = uniq_programs[psample[(as.integer(n*0.75)+1):n]]
+  np = length(uniq_programs)
+  #print(paste("number of programs",,"selected with at most", n,"events"))
 
-  buggy_train = buggy_cases[buggy_cases$program %in% train_programs,names(buggy_cases) != "program"]
-  buggy_test  = buggy_cases[buggy_cases$program %in% test_programs,names(buggy_cases) != "program"]
+  for (r in 1:nrep) {
+    
+    gc() 
 
-  robust_train = robust_cases[robust_cases$program %in% train_programs,names(robust_cases) != "program"]
-  robust_test  = robust_cases[robust_cases$program %in% test_programs,names(robust_cases) != "program"]
+    psample = sample(np)
+    train_programs = uniq_programs[psample[1:as.integer(np*0.75)]]
+    test_programs = uniq_programs[psample[(as.integer(np*0.75)+1):np]]
 
-  train_size = min(nrow(buggy_train), nrow(robust_train))
-  test_size = min(nrow(buggy_test), nrow(robust_test))
+    print(c("intersection train and test:", intersect(train_programs,test_programs)))
 
-  print(train_size)
-  print(test_size)
+    buggy_train = buggy_cases[buggy_cases$program %in% train_programs,names(buggy_cases) != "program"]
+    buggy_test  = buggy_cases[buggy_cases$program %in% test_programs,names(buggy_cases) != "program"]
 
-  #print(levels(factor(robust_cases$program)))
-  #print(levels(factor(buggy_cases$program)))
-  #aaaa
+    robust_train = robust_cases[robust_cases$program %in% train_programs,names(robust_cases) != "program"]
+    robust_test  = robust_cases[robust_cases$program %in% test_programs,names(robust_cases) != "program"]
+ 
+    buggy_train = sample(buggy_train)
+    buggy_test = sample(buggy_test)
 
+    robust_train = sample(robust_train)
+    robust_test = sample(robust_test)
 
-  #train_size = min(500,as.integer(nrow(buggy_cases)*0.75))
-  #test_size = min(250,nrow(buggy_cases) - train_size)
+    train_size = min(nrow(buggy_train), nrow(robust_train))
+    test_size = min(nrow(buggy_test), nrow(robust_test))
 
-  #print(train_size)
-  #print(test_size)
+    print(c(train_size, test_size))
 
-  #n = nrow(buggy_cases)
-  #rsample = 1:n
-  #rsample = sample(n)
+    train = rbind(buggy_train[1:train_size,], robust_train[1:train_size,])
+    test  = rbind(buggy_test[1:test_size,], robust_test[1:test_size,])
 
-  #train_sample = rsample[1:(train_size)] 
-  #test_sample = rsample[(train_size+1):(train_size+test_size)]
+    varnot0 = names(train)[unlist(lapply(train,function(x) 0 != var(x)))]
 
-  #print(rsample)
+    xy_train = train[,varnot0]
+    xy_test  = test[,varnot0]
 
-  #buggy_train = buggy_cases[train_sample,]
-  #buggy_test  = buggy_cases[test_sample,]
+    x_test = test[,names(test) != "class"]
+    y_test  = test[,"class"]
 
-  #print(nrow(buggy_train))
-  #print(nrow(buggy_test))
+    tcont = tune.control(sampling='fix')
+    #print(names(xy_train))
+    m = tune.svm(class~., data = xy_train, validation.x = x_test, validation.y = y_test, gamma = 10^(-5:-1), cost = 10^(-1:3), tunecontrol=tcont)
+    model = m$best.model
+    #model = svm(class ~., data=xy_train, gamma=1e-05, cost=0.1)    
 
-  # robust train and test
-
-  #n = nrow(robust_cases)
-  #rsample = 1:n
-  #rsample = sample(n)
-
-  #print(rsample)
-
-  # n cases are selected to keep the train dataset balanced
-  #train_sample = rsample[1:(train_size)]
-  #test_sample =  rsample[(train_size+1):(train_size+test_size)]
-  #more_test_sample = rsample[(train_size+test_size+1):n]
-
-  #robust_train = robust_cases[train_sample,]
-  #robust_test  = robust_cases[test_sample,]
-  #robust_more_test = robust_cases[more_test_sample,]
-
-  #print(nrow(robust_train))
-  #print(nrow(robust_test))
+    z = predict(model,x_test)
+    #print(summary(m))
+    print(confusionMatrix(table(pred=z, true=y_test)))
+    mut_only_acc = mut_only_acc + as.numeric(confusionMatrix(table(pred=z, true=y_test))$overall[1])
+    print(mut_only_acc / r)
+  }
   
-  train = rbind(buggy_train, robust_train)
-  test  = rbind(buggy_test, robust_test)
-
-  varnot0 = names(train)[unlist(lapply(train,function(x) 0 != var(x)))]
-
-  xy_train = train[,varnot0]
-  xy_test  = test[,varnot0]
-
-  x_test = test[,names(test) != "class"]
-  y_test  = test[,"class"]
-
-  tcont = tune.control(sampling='fix')
-  #print(names(xy_train))
-  m = tune.svm(class~., data = xy_train, validation.x = x_test, validation.y = y_test, gamma = 10^(-5:-1), cost = 10^(-1:3), tunecontrol=tcont)
-  
-  model = m$best.model
-  z = predict(model,x_test)
-  print(summary(m))
-  res = c(res,confusionMatrix(table(pred=z, true=y_test))$overall[1])
+  res = c(res, mut_only_acc / nrep)    
 
 }
 
